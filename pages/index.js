22718ccfb1d3fbe6bc2d46b7a8b039088bcd6c39
Ali Ahmed <aliahmed@Alis-MacBook-Pro.local>
2023-01-11 18:06:03 -0800

    committing to it

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
+
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
   return (
     <div className="root">
       <Head>
@@ -11,11 +18,21 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Explain it to me like im 5</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>The teacher you still need</h2>
           </div>
+        </div>
+        <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box" value={userInput} onChange={onUserChangedText} />
+          <div className="prompt-buttons">
+    <a className="generate-button" onClick={null}>
+      <div className="generate">
+        <p>Generate</p>
+      </div>
+    </a>
+  </div>
         </div>
       </div>
       <div className="badge-container grow">
